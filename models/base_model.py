5c7776fcc0bdd4286636e402d75d44c6efe73a23
mariam salama <mariamsalama888@gmail.com>
2023-12-09 15:23:17 -0800

    base

--- models/base_model.py
+++ models/base_model.py
@@ -14,14 +14,6 @@ class BaseModel:
         self.id = str(uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    self.__dict__[key] = datetime.strptime(value, time_format)
-                else:
-                    self.__dict__[key] = value
-        else:
-            models.storage.new(self)
 
     def save(self):
         """Update updated_at with the current datetime."""
