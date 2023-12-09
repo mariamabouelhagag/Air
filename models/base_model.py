844358fc2c6b889b8a068818029f33c42f3ec36e
mariam salama <mariamsalama888@gmail.com>
2023-12-09 15:56:11 -0800

    base

--- models/base_model.py
+++ models/base_model.py
@@ -1,47 +1,46 @@
 #!/usr/bin/python3
-"""Defines the BaseModel class."""
-import models
+"""Module for BaseModel class"""
 from uuid import uuid4
 from datetime import datetime
 
+import models
+
 
 class BaseModel:
-    """Represents the BaseModel of the HBnB project."""
+    """BaseModel Class"""
 
     def __init__(self, *args, **kwargs):
-        """Initialize a new BaseModel."""
-        time_format = "%Y-%m-%dT%H:%M:%S.%f"
-        self.id = str(uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
-        model.storage.new(self)
-
+        """Initializes a new BaseModel class."""
+        time_format = "%Y-%m-%d %H:%M:%S.%f"
         if kwargs:
-            for key, value in kwargs.item():
-            if key == "created_at" or key == "updated_at":
-                 self.__dict__[key] = datetime.strptime(value, time_format)
-                else:
-                    self.__dict__[key] = value
+            self.id = str(uuid4())
+            self.created_at = datetime.today()
+            self.updated_at = datetime.today()
+            models.storage.new(self)
 
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                if key in ['created_at', 'updated_at']:
+                    setattr(self, key,
+                            datetime.strptime(value, time_format))
+                else:
+                    setattr(self, key, value)
+
+    def __str__(self):
+        """Return a string representation of the BaseModel."""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """Update updated_at with the current datetime."""
-        self.updated_at = datetime.today()
+        """Save the current state of the BaseModel."""
+        self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        """Return the dictionary of the BaseModel instance.
-
-        Includes the key/value pair __class__ representing
-        the class name of the object.
-        """
+        """Return a dictionary representation of the BaseModel."""
         return {
-          'id': self.id,
-          '__class__': self.__class__.__name__,
-          'created_at': str(self.created_at),
-          'updated_at': str(self.updated_at)
+            'id': self.id,
+            '__class__': self.__class__.__name__,
+            'created_at': str(self.created_at),
+            'updated_at': str(self.updated_at)
         }
-
-    def __str__(self):
-        """Return the print/str representation of the BaseModel instance."""
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
