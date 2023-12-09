8d46254c2d030aa41dfd20c7f0781671404a3d18
mariam salama <mariamsalama888@gmail.com>
2023-12-09 09:10:37 -0800

    base

--- models/base_model.py
+++ models/base_model.py
@@ -11,6 +11,7 @@ class BaseModel:
     Base class for all models
     """
     def __init__(self):
+      time_format = "%Y-%m-%dT%H:%M:%S.%f" 
       self.id = str(uuid.uuid4())
       self.created_at = datetime.today()
       self.updated_at = datetime.today()
