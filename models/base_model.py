3b110623ca002e2849d6a830c0b4b0f8464647dc
mariam salama <mariamsalama888@gmail.com>
2023-12-09 10:32:48 -0800

    base

--- models/base_model.py
+++ models/base_model.py
@@ -15,14 +15,6 @@ class BaseModel:
       self.id = str(uuid.uuid4())
       self.created_at = datetime.today()
       self.updated_at = datetime.today()
-        if len(kwargs) != 0:
-            for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    self.__dict__[key] = datetime.strptime(value, time_format)
-                else:
-                    self.__dict__[key] = value
-        else:
-            models.storage.new(self)
 
 
     def save(self):
@@ -30,7 +22,6 @@ class BaseModel:
       Updates the updated_at attribute
       """
       self.updated_at = datetime.today()
-      models.storage.save()
 
     def __str__(self):
       """
