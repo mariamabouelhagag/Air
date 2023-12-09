95bb115a4bf308cc22133a0e0d0eaefe64c7c7a0
mariam salama <mariamsalama888@gmail.com>
2023-12-09 15:29:53 -0800

    storage

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,7 @@
-#!user/bin/python3
-"""
-initalization method
-"""
+#!/usr/bin/python3
+"""__init__ magic method for models directory"""
+from models.engine.file_storage import FileStorage
 
-from  models.base_model import BaseModel
+
+storage = FileStorage()
+storage.reload()
