62fdba8b8286aa7d58020ff4aead05790208177e
mariam salama <mariamsalama888@gmail.com>
2023-12-10 08:37:32 -0800

    p

--- console.py
+++ console.py
@@ -6,11 +6,11 @@ class HBNBCommand(cmd.Cmd):
 
     prompt = '(hbnb) '
 
-    def do_quit(self, arg):
+    def do_quit(self, line):
         """Quit command to exit the program"""
         return True
 
-    def do_EOF(self, arg):
+    def do_EOF(self, line):
         """EOF command to exit the program"""
         return True
 
