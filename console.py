7d4017107c5ebd21011c45c46d10df59a397cee5
mariam salama <mariamsalama888@gmail.com>
2023-12-10 08:28:15 -0800

    prompt

---
+++ console.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """HBNBCommand class that includes methods for our command interpreter."""
+
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF command to exit the program"""
+        return True
+
+    def emptyline(self):
+        """An empty line + ENTER shouldn’t execute anything"""
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
